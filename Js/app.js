0f5ec8b04dd975e69ebc8d608b83cf606f681c25
Kkaguamba <kkaguamba@gmail.com>
2023-10-28 09:34:32 +0300

    Vs commit

---
+++ Js/app.js
@@ -0,0 +1,33 @@
+
+let signupBtn = document.getElementById("signupBtn");
+    let signinBtn = document.getElementById("signinBtn");
+    let nameField = document.getElementById("nameField");
+    let title = document.getElementById("title");
+
+    signinBtn.onclick = function(){
+        nameField.style.maxHeight = "0";
+        title.innerHTML = "Sign In";
+        signupBtn.classList.add('disable');
+        signinBtn.classList.remove('disable');
+    }
+
+    signupBtn.onclick = function(){
+        nameField.style.maxHeight = "60px";
+        title.innerHTML = "Sign Up";
+        signupBtn.classList.remove('disable');
+        signinBtn.classList.add('disable');
+    }
+
+    var a;
+    function pass() {
+        if(a==1){
+            document.getElementById("password").type="password";
+            document.getElementById("pass-icon").src="images/hidden.png";
+            a=0;
+        }
+        else{
+            document.getElementById("password").type="text";
+            document.getElementById("pass-icon").src="images/eye.png";
+            a=1;
+        }
+    }
